2e45e147564a62904841cbbcecb0e3c431bb1790
vuphan1101 <vuphan1101@gmail.com>
2025-11-05 11:10:20 -0600

    second commit

---
+++ greet.py
@@ -0,0 +1,8 @@
+# Create a program that receive a parameter and prints "Hello <parameter>",
+# it should be a return statement
+
+def greet(name):
+    print(f"{name} says hello")
+
+
+#create a new file and call this method in that file, and print it, print the output.
---
+++ main.py
@@ -0,0 +1,8 @@
+#import greet
+#print(greet.greet("Vu"))
+
+#from greet import greet
+#print(greet("Vu"))
+
+import math
+print(dir(math))
